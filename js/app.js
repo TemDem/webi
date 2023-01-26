6d85c9db9ea4089215ab6a1808e33b51a8650174
TemDem <86498940+TemDem@users.noreply.github.com>
2023-01-26 19:42:45 +0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -5019,7 +5019,7 @@
         da.init();
         const mask = document.querySelector(".mask");
         const tel = document.querySelector(".input_tel");
-        if (mask) window.addEventListener("DOMContentLoaded", (function(e) {
+        if (mask) window.addEventListener("load", (function(e) {
             mask.classList.remove("_active");
             console.log(2345);
         }));
